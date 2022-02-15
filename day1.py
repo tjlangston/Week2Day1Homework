3a61f2d384123e3db6dd61cd03273caca8628eec
tjlangston <tjlangston@gmail.com>
2022-02-15 09:20:50 -0500

    created

---
+++ day1.py
@@ -0,0 +1,8 @@
+# 1. Cube Number Test... 
+#Print out all cubed numbers up to the total value 1000. 
+#Meaning that if the cubed number is over 1000 break the loop.
+
+# 2. Get first prime numbers up to 100
+
+# 3. Take in a users input for their age, if they are younger than 18 print kids,
+#if they're 18 to 65 print adults, else print seniors
